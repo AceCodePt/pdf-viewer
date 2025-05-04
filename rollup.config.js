6459801513736006bf018f0818da0ae882d0784c
Sagi Carmel <ace.code.pt@gmail.com>
2025-05-04 14:28:56 +0300

    feat: made it moduled

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,23 @@ import terser from "@rollup/plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: "pdf-viewer.ts", // Update input path
+  input: "pdf-viewer.mts",
   output: [
     {
-      file: "dist/pdf-viewer.bundle.js",
+      file: "dist/pdf-viewer.bundle.cjs",
+      format: "iife",
+    },
+    {
+      file: "dist/pdf-viewer.bundle.min.cjs",
+      format: "iife",
+      plugins: [terser()],
+    },
+    {
+      file: "dist/pdf-viewer.bundle.mjs",
       format: "es",
     },
     {
-      file: "dist/pdf-viewer.bundle.min.js",
+      file: "dist/pdf-viewer.bundle.min.mjs",
       format: "es",
       plugins: [terser()],
     },
